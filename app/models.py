206b77cb095315f3be5e97222db84dcdea2b59a3
JetBaget <jetneitrino@gmail.com>
2018-07-09 17:58:17 +0300

    Реализовал добавление данных в шаблон из базы

--- app/models.py
+++ app/models.py
@@ -1,25 +1,8 @@
 from app import db
-import json
+from sqlalchemy.dialects import postgresql
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
-
-
-class JsonEncodedDict(db.TypeDecorator):
-    """Enables JSON storage by encoding and decoding on the fly."""
-    impl = db.Text
-
-    def process_bind_param(self, value, dialect):
-        if value is None:
-            return '{}'
-        else:
-            return json.dumps(value)
-
-    def process_result_value(self, value, dialect):
-        if value is None:
-            return {}
-        else:
-            return json.loads(value)
 
 
 class Category(db.Model):
@@ -34,12 +17,12 @@ class Category(db.Model):
 class Product(db.Model):
     __tablename__ = 'product'
     id = db.Column(db.Integer, primary_key=True)
-    category = db.relationship('Category', backref=db.backref('product', lazy=True))
-    category_id = db.Column(db.Integer, db.ForeignKey("category.id"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
     title = db.Column(db.String(64), index=True, unique=True)
     description = db.Column(db.String(), index=True)
-    params = db.Column(db.JSON(), index=True)
+    params = db.Column(postgresql.JSONB(astext_type=False), index=True)
     img_path = db.Column(db.String(64), index=True, unique=True)
+    category = db.relationship('Category', backref=db.backref('product', lazy=True))
 
     def __repr__(self):
         return '<Post %r>' % self.title
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.models import Product, Category
 
 
 @app.route('/')
@@ -8,24 +9,13 @@ def main():
     return render_template('main.html', title='Главная')
 
 
-@app.route('/products/', methods=['GET', 'POST'])
+@app.route('/products/')
 def products_list():
-    prod_set = [{'header': 'IPhone 7 (100500Гб, полный фарш)',
-                 'params': ['Объем памяти: 64Гб', 'Цвет: Черный', 'Диагональ экрана: 5.5`'],
-                 'img_path': '/static/img/iphone7.jpg',
-                 'desc': 'Некуда девать лишние деньги? Тогда это лучший выбор!'},
-                {'header': 'Macbook Air 2018 (16Гб оперативы)',
-                 'params': ['Объем оперативной памяти: 16Гб', 'Жёсткий диск: 500Гб', 'Цвет: Серебристый',
-                            'Диагональ экрана: 17`'],
-                 'img_path': '/static/img/macbook.jpg',
-                 'desc': 'Охурменный ноутбук за нехилое баблище'}]
+    prod_set = list(Product.query.all())
     return render_template('products_list.html', products=prod_set)
 
 
 @app.route('/products/<product_id>')
 def product(product_id):
-    prod = {'header': 'IPhone 7 (100500Гб, полный фарш)',
-            'params': ['Объем памяти: 64Гб', 'Цвет: Черный', 'Диагональ экрана: 5.5`'],
-            'img_path': '/static/img/iphone7.jpg',
-            'desc': 'Некуда девать лишние деньги? Тогда это лучший выбор!'}
+    prod = None
     return render_template('product_item.html', prod=prod)
