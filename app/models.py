62e913d67415403fba722d8134f28e76830755d0
Khromov Anton <jetneitrino@gmail.com>
2018-07-09 03:25:55 +0300

    Добавил json поле в БД, сделал связь между страницами

--- app/models.py
+++ app/models.py
@@ -1,18 +1,45 @@
 from app import db
+import json
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
 
+class JsonEncodedDict(db.TypeDecorator):
+    """Enables JSON storage by encoding and decoding on the fly."""
+    impl = db.Text
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return '{}'
+        else:
+            return json.dumps(value)
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return {}
+        else:
+            return json.loads(value)
+
+
 class Category(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
 
 
 class Product(db.Model):
+    __tablename__ = 'product'
     id = db.Column(db.Integer, primary_key=True)
+    category = db.relationship('Category', backref=db.backref('product', lazy=True))
     category_id = db.Column(db.Integer, db.ForeignKey("category.id"), nullable=False)
-    name = db.Column(db.String(64), index=True, unique=True)
+    title = db.Column(db.String(64), index=True, unique=True)
     description = db.Column(db.String(), index=True)
-    params = db.Column(db.String())
+    params = db.Column(db.JSON(), index=True)
     img_path = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
--- app/views.py
+++ app/views.py
@@ -5,17 +5,27 @@ from app import app
 @app.route('/')
 @app.route('/main/')
 def main():
-    return render_template('index.html', title='Главная')
+    return render_template('main.html', title='Главная')
 
 
 @app.route('/products/', methods=['GET', 'POST'])
-def product():
+def products_list():
     prod_set = [{'header': 'IPhone 7 (100500Гб, полный фарш)',
                  'params': ['Объем памяти: 64Гб', 'Цвет: Черный', 'Диагональ экрана: 5.5`'],
                  'img_path': '/static/img/iphone7.jpg',
                  'desc': 'Некуда девать лишние деньги? Тогда это лучший выбор!'},
                 {'header': 'Macbook Air 2018 (16Гб оперативы)',
-                 'params': ['Объем оперативной памяти: 16Гб', 'Жёсткий диск: 500Гб', 'Цвет: Серебристый', 'Диагональ экрана: 17`'],
+                 'params': ['Объем оперативной памяти: 16Гб', 'Жёсткий диск: 500Гб', 'Цвет: Серебристый',
+                            'Диагональ экрана: 17`'],
                  'img_path': '/static/img/macbook.jpg',
                  'desc': 'Охурменный ноутбук за нехилое баблище'}]
-    return render_template('item.html', products=prod_set)
+    return render_template('products_list.html', products=prod_set)
+
+
+@app.route('/products/<product_id>')
+def product(product_id):
+    prod = {'header': 'IPhone 7 (100500Гб, полный фарш)',
+            'params': ['Объем памяти: 64Гб', 'Цвет: Черный', 'Диагональ экрана: 5.5`'],
+            'img_path': '/static/img/iphone7.jpg',
+            'desc': 'Некуда девать лишние деньги? Тогда это лучший выбор!'}
+    return render_template('product_item.html', prod=prod)
